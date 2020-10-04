98ce2fe165d16369b09294402aa62cf3aad6f5eb
kohkb <koh.plate@gmail.com>
2020-10-04 13:00:44 +0900

    update db.go

---
+++ db/db.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func Init() *gorm.DB {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("データベースへの接続に失敗しました")
+	}
+	db.LogMode(true)
+	// db.AutoMigrate((&models.Book{}))
+	return db
+}
