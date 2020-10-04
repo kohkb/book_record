1a951a5707377b89dbed5451ff450b089db91cc6
kohkb <koh.plate@gmail.com>
2020-10-04 21:39:49 +0900

    Add api

---
+++ api/v1/book.go
@@ -0,0 +1,68 @@
+package v1
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/kohkb/book_record/models"
+)
+
+type BooksHandler struct {
+	Db *gorm.DB
+}
+
+func (h *BooksHandler) GetAll(c *gin.Context) {
+	var books []models.Book
+	h.Db.Find(&books)
+	c.JSON(http.StatusOK, books)
+}
+
+func (h *BooksHandler) CreateTask(c *gin.Context) {
+	book := models.Book{}
+	err := c.BindJSON(&book)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	h.Db.Create(&book)
+	c.JSON(http.StatusOK, &book)
+}
+
+func (h *BooksHandler) EditTask(c *gin.Context) {
+	book := models.Book{}
+	id := c.Param("id")
+	h.Db.First(&book, id)
+	c.JSON(http.StatusOK, book)
+}
+
+func (h *BooksHandler) UpdateTask(c *gin.Context) {
+	book := models.Book{}
+	id := c.Param("id")
+	h.Db.First(&book, id)
+	err := c.BindJSON(&book)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+	h.Db.Save(&book)
+	c.JSON(http.StatusOK, &book)
+}
+
+func (h *BooksHandler) DeleteTask(c *gin.Context) {
+	book := models.Book{}
+	id := c.Param("id")
+	h.Db.First(&book, id)
+	err := h.Db.First(&book, id).Error
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	h.Db.Delete(&book)
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
+}
--- router/router.go
+++ router/router.go
@@ -1,8 +1,12 @@
 package router
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	v1 "github.com/kohkb/book_record/api/v1"
 	"github.com/kohkb/book_record/controllers"
 )
 
@@ -11,11 +15,34 @@ func Router(dbConn *gorm.DB) {
 		Db: dbConn,
 	}
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "DELETE", "POST", "GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "*"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	r.LoadHTMLGlob("templates/*")
-	r.GET("/books", booksHandler.GetAll)                 // 一覧画面
-	r.POST("/books", booksHandler.CreateTask)            // 新規作成
-	r.GET("/books/:id", booksHandler.EditTask)           // 編集画面
-	r.POST("/books/edit/:id", booksHandler.UpdateTask)   // 更新
-	r.POST("/books/delete/:id", booksHandler.DeleteTask) // 削除
+	r.GET("/books", booksHandler.GetAll)
+	r.POST("/books", booksHandler.CreateTask)
+	r.GET("/books/:id", booksHandler.EditTask)
+	r.POST("/books/edit/:id", booksHandler.UpdateTask)
+	r.POST("/books/delete/:id", booksHandler.DeleteTask)
+	apiV1 := r.Group("/api/v1")
+	{
+		apiBooksHandler := v1.BooksHandler{
+			Db: dbConn,
+		}
+		apiV1.GET("/books", apiBooksHandler.GetAll)
+		apiV1.POST("/books", apiBooksHandler.CreateTask)
+		apiV1.GET("/books/:id", apiBooksHandler.EditTask)
+		apiV1.PUT("/books/:id", apiBooksHandler.UpdateTask)
+		apiV1.DELETE("/books/:id", apiBooksHandler.DeleteTask)
+	}
+
 	r.Run()
 }
